9a477d96a407c58882f677148be2c92298073577
Chebihi <chebihi.infinitysoft@google.com>
2022-01-22 23:18:01 +0000

    Initial commit

---
+++ team_management/src/test/java/ma/infinitysof/teammanagement/TeamManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.infinitysof.teammanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
